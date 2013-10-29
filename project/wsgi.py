67dd3fa9b5624a27174f3d1d50e7bbf0f693d5e2
Kristian Øllegaard <kristian@kristian.io>
2013-10-29 17:04:21 +0100

    serve static media

--- project/wsgi.py
+++ project/wsgi.py
@@ -25,9 +25,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from dj_static import Cling, MediaCling
 
-application = Cling(get_wsgi_application())
+#application = Cling(get_wsgi_application())
+application = Cling(MediaCling(get_wsgi_application()))
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
